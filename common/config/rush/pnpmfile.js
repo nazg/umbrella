ded3b184d2c28f9e52274a5457f4e130f8269cd2
jjlorenzo <jjlorenzo@gmail.com>
2020-03-24 23:25:39 -0500

    rush(root): init

---
+++ common/config/rush/pnpmfile.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  hooks: {
+    // readPackage: function(pkg, context) {
+    //   if (pkg.name === "foo") {
+    //    pkg.dependencies["bar"] = "baz"
+    //    context.log("noop")
+    //   }
+    //   return pkg
+    // },
+  }
+}
